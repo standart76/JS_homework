ca9aa6b8a0fdd28ac37f44cf2c805605a3c5f087
Sharov Ivan <standart76@yandex.ru>
2020-10-12 13:37:12 +0300

    Правка объявления переменных

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-var money; 
-var income;
-var addExpenses;
-var deposit;
-var mission;
-var period;
+let money; 
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
 alert("Hello world! Here is lesson01!");
 
