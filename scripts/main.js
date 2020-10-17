cd46aba77f5ed06190cdb553555efa3193f2254f
Sharov Ivan <standart76@yandex.ru>
2020-10-17 19:27:33 +0300

    Шестой урок. Правка функции с замыканием

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,37 @@
 'use strict';
 
-let numberPC = Math.floor(Math.random() * 100);
-console.log(numberPC);
-let numberUser = 0;
+let getRandom = function(){
+	return  Math.floor(Math.random() * 101);
+};
 
 let isNumber = function(n){
 	return !isNaN(n) && isFinite(n);
 };
 
-	
-
-let start = function(){
-	
-	numberUser = prompt('Угадай число от 1 до 100');
-	if(numberUser === null){//отмена ввода
-		alert('Игра окончена');
-		return '';
-	}
-	if(!isNumber(numberUser)){//проверка ввода на формат числа
-		alert('Введи число!');
-		return start();
-	}
-	if(numberUser > numberPC){
-		alert('Загаданное число меньше');
-		return start();
-	}
-	if(numberUser < numberPC){
-		alert('Загаданное число больше');
-		return start();
-	}
-	return alert('Поздравляю, Вы угадали!!!');
+let game = function(){
+	let numberPC = getRandom();
+	return function play(){
+		let numberUser = prompt('Угадай число от 1 до 100');
+		if(numberUser === null){//отмена ввода
+			alert('Игра окончена');
+			return '';
+		}
+		if(!isNumber(numberUser)){//проверка ввода на формат числа
+			alert('Введи число!');
+			return play();
+		}
+		if(numberUser > numberPC){
+			alert('Загаданное число меньше');
+			return play();
+		}
+		if(numberUser < numberPC){
+			alert('Загаданное число больше');
+			return play();
+		}
+		return alert('Поздравляю, Вы угадали!!!');
+	};
 };
 
-start();
+let game1 = game();
+
+game1();
